a218078e0f36812f312a6dc4961a654536e97e1e
Harsh Jain <harshj.amity@gmail.com>
2018-03-27 01:40:37 +0530

    Hackerrank LeftRotation

--- src/com/example/practice/hackerrank/LeftRotation.java
+++ src/com/example/practice/hackerrank/LeftRotation.java
@@ -8,8 +8,7 @@ public class LeftRotation {
 
         int temp;
 
-        for(int i=0;i<k;i++)
-        {
+        for(int i=0;i<k;i++) {
             temp = a[0];
             for (int j=0;j<n-1;j++)
             {
