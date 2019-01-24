8004027077aa139cbb2c337f57ee63beb7cad698
Harsh Jain <hiharshjain@gmail.com>
2019-01-24 16:08:27 +0530

    fix

--- src/self/LCS.java
+++ src/self/LCS.java
@@ -1,17 +1,15 @@
 package self;
 
-import java.util.Arrays;
-
 public class LCS {
 
     static String first = "bbbb";
-    static String second = "ccccc";
+    static String second = "ccbbcc";
 
-    static int arr[][] = new int[4][5];
+    static int arr[][] = new int[first.length() + 1][second.length() + 1];
 
     public static void main(String[] args) {
-        for (int i=0;i<4;i++) {
-            for (int j=0;j<5;j++) {
+        for (int i = 0; i < first.length() + 1; i++) {
+            for (int j = 0; j < second.length() + 1; j++) {
                 arr[i][j] = -1;
             }
         }
@@ -19,7 +17,7 @@ public class LCS {
         System.out.println(dpLCS(first.toCharArray(), second.toCharArray(), 4, 5));
     }
 
-    static int recurseLCS(char[] P, char[] Q, int n, int m) {
+    private static int recurseLCS(char[] P, char[] Q, int n, int m) {
         if (m == 0 || n == 0) {
             return 0;
         } else if (P[n - 1] == Q[m - 1]) {
@@ -29,23 +27,21 @@ public class LCS {
         }
     }
 
-    static int dpLCS(char[] P, char[] Q, int n, int m) {
+    private static int dpLCS(char[] P, char[] Q, int n, int m) {
         int result;
 
-        if (arr[n-1][m-1]!=-1)
-            return arr[n-1][m-1];
+        if (arr[n][m] != -1)
+            return arr[n][m];
         if (m == 0 || n == 0) {
             result = 0;
         } else if (P[n - 1] == Q[m - 1]) {
-            result = 1 + recurseLCS(P, Q, n - 1, m - 1);
+            result = 1 + dpLCS(P, Q, n - 1, m - 1);
         } else {
-            result = Math.max(recurseLCS(P, Q, n - 1, m), recurseLCS(P, Q, n, m - 1));
+            result = Math.max(dpLCS(P, Q, n - 1, m), dpLCS(P, Q, n, m - 1));
         }
 
-        arr[n-1][m-1] = result;
+        arr[n][m] = result;
 
         return result;
     }
-
-
 }
